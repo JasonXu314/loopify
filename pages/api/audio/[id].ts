c5081e8e3586ee5505a815244a60727b38e334ae
Jason Xu <Xinwen31415@gmail.com>
2021-06-26 22:40:37 -0500

    testing

--- pages/api/audio/[id].ts
+++ pages/api/audio/[id].ts
@@ -9,18 +9,25 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
 		const gridFS = new GridFSBucket(db);
 		const id = req.query.id as string | undefined;
 
+		console.log('hi');
 		const video = await db.collection<Video>('metadata').findOne({ _id: id });
 
+		console.log('hi2');
 		if (video) {
+			console.log('hi3');
 			res.status(200).setHeader('Content-Disposition', contentDisposition(`${video.title}.mp3`));
+			console.log('hi4');
 			const dlStream = gridFS.openDownloadStream(id as unknown as ObjectId);
 
+			console.log('hi5');
 			dlStream.pipe(res);
 
+			console.log('hi6');
 			await new Promise((resolve) => {
 				dlStream.on('close', resolve);
 				dlStream.on('end', resolve);
 			});
+			console.log('hi7');
 		} else {
 			res.status(404).send('No video with that id found');
 		}
